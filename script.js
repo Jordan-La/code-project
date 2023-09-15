a7259ea96028aef9e49f326278866e7baf618603
Jordan-La <107329389+Jordan-La@users.noreply.github.com>
2023-09-14 19:02:14 -0700

    Working JS Code Given Stop, Activate, and an input - Jordan Lam

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-wadaiohawihdawihdaihdaiwhhi
-bjndniwdniaw
+var userInput = document.getElementById("userInput");
+userInput = 60;
+var timeRemaining;
+
+if(isNaN(userInput)){
+    console.log("lowkey this aint a number");
+}else{
+    timeRemaining = userInput;
+}
+
+var buttonActivate = document.getElementById("activationButton").addEventListener("click", beginCountDown());
+var buttonStop = document.getElementById("stopButton").addEventListener("click", stop());
+var buttonReset = document.getElementById("resetButton").addEventListener("click", beginCountDown(countDown));
+var stop = false;
+
+timeRemaining = userInput;
+function beginCountDown(){
+    while(timeRemaining > 0 && (shutdown == false)){
+        timeRemaining = timeRemaining - 1;
+    }
+    alert("yo ur times out lmao");
+}
+function stop(){
+    stop = true;
+}
+
