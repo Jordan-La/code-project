530b7b275230d0168fcf0ecedc4345fd55aa7d8e
Jordan <rubixcubes227@gmail.com>
2023-09-20 22:13:00 -0700

    server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.get("/", function(req,res) {
     res.send("yo")
-	res.sendFile(__dirname + "/code-project/index.html");
+res.sendFile(__dirname + "/Jordan-La/code-project/index.html");
 });
 app.post("/", function(req,res) {
     res.sendFile(__dirname + "/code-project/index.html");
