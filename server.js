be4a1df99d8c8dcd0ebde62ff36405e0a7a0ba81
Jordan-La <107329389+Jordan-La@users.noreply.github.com>
2023-09-20 22:22:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require("express"); 
 const app = express();
 app.get("/", function(req,res) {
-res.sendFile(__dirname + "/code-project/index.html");
+res.sendFile(/code-project/index.html");
 });
 app.post("/", function(req,res) {
-    res.sendFile(__dirname + "/code-project/index.html");
+    res.sendFile("/code-project/index.html");
 });
 
 app.listen(process.env.PORT, function() {
