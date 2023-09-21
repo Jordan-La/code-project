fe4bd4f4e3c4e38f949f5469d1d22bde04e670a4
Jordan-La <107329389+Jordan-La@users.noreply.github.com>
2023-09-20 22:19:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express"); 
 const app = express();
 app.get("/", function(req,res) {
-    res.send("yo")
 res.sendFile(__dirname + "/code-project/index.html");
 });
 app.post("/", function(req,res) {
