cc098873c26384b16c45b89ebfc8c88fc47f2030
Jordan <rubixcubes227@gmail.com>
2023-09-20 21:19:03 -0700

    1

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-
+app.get("/", function(req,res) {
+    res.sendFile("https://vscode.dev/github.com/Jordan-La/code-project/blob/main/script.js#L21");
+});
 app.listen(process.env.PORT, function() {
     console.log(`Server is running on port ${process.env.PORT}.`);
 });
