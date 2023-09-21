c46a04f2f180c5adce160eb8d4fa412fb04b48a9
Jordan <rubixcubes227@gmail.com>
2023-09-20 20:53:02 -0700

    pls work

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-testing
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Server is running on port ${process.env.PORT}.`);
+});
+
