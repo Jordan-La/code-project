9d267554942ad237f4a6b70ba7faae44b0834c23
Jordan <rubixcubes227@gmail.com>
2023-09-20 22:02:08 -0700

    server

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 
 const express = require("express"); 
 const app = express();
-
-app.use(express.static("./public"));
 app.get("/", function(req,res) {
-    res.send("GET Request Called");
+    res.send("yo")
 });
 app.post("/", function(req,res) {
-    res.sendFile(__dirname + "Jordan-La/code-project/index.html");
+    res.sendFile(__dirname + "/code-project/index.html");
 });
+
 app.listen(process.env.PORT, function() {
     console.log(`Server is running on port ${process.env.PORT}.`);
 });
