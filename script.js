5147feb0f5986302cdb8d11ce41b8443373f88f9
Jordan-La <107329389+Jordan-La@users.noreply.github.com>
2023-09-21 20:24:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 var userInput = document.getElementById("userInput");
-//userInput = 60; // some test code
-var buttonActivate = document.getElementById("activationButton").addEventListener("click", setInterval(beginCountDown, 1000));
-var buttonStop = document.getElementById("stopButton").addEventListener("click", stop());
-var buttonReset = document.getElementById("resetButton").addEventListener("click", reset());
+var display = document.getElementById("display");
+var time = 0;
+var outputTime = 0;
+
+var buttonActivate = document.getElementById("activationButton");
+
+buttonActivate.addEventListener("click", function(){
+        time = userInput.value;
+       outputTime = setInterval(beginCountDown, 1000)
+       display.innerHTML = 0;
+});
+    
+var buttonStop = document.getElementById("stopButton");
+
+buttonStop.addEventListener("click", function(){
+        stopTime();
+        console.log("Stopping time");
+});
+
+var buttonReset = document.getElementById("resetButton");
+
+buttonReset.addEventListener("click", function(){
+        reset();
+        console.log("Resetting time");
+});
+
 var stop = false;
 function beginCountDown(){
-    userInput--;
-    console.log("-1 second")
-    if(userInput == 0 || stop == true){
-        alert("yo ur times out lmao");
-        return;
+    if(time == 0 || stop == true){
+        console.log("final time: " + time);
+        display.innerHTML = time;
+        alert("no mo time");
+        clearInterval(outputTime);
+        return userInput;
     }
+    console.log("user inputted: " + time);
+    display.innerHTML = time;
+    console.log("1 second subtracted")
+    time--;
 }
-function stop(){
-    stop = true;
+function stopTime(){
+    clearInterval(outputTime);
 }
 function reset(){
-    userInput = 0;
-
+    time = 0;
+    display.innerHTML = time;
+    clearInterval(outputTime);
 }
-//this is a testing
-
+//this is a testingj
